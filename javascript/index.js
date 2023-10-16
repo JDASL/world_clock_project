1a45f6208677b541fc1e33571cb34aa56e1f01f9
JDASL <127839733+JDASL@users.noreply.github.com>
2023-10-16 12:43:13 +0200

    fixed div class to id
    problem solved

--- javascript/index.js
+++ javascript/index.js
@@ -36,13 +36,13 @@ function updateCity(event) {
   citiesElement.innerHTML = `
   <div class="city">
     <div>
-        <h2>${cityName}</h2>
-        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
-        <div class="time">${cityTime.format(
-          "h:mm:ss"
-        )} <small>${cityTime.format("A")}</small></div>
-    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
   `;
 }
 
