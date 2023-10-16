230d76542d93505487053d46b3bfd779a749f69a
JDASL <127839733+JDASL@users.noreply.github.com>
2023-10-16 12:31:12 +0200

    updated citiesElement.innerHTML

--- javascript/index.js
+++ javascript/index.js
@@ -27,9 +27,20 @@ function updateCityTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
-  citiesSelectElement.innerHTML = "Blah";
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+    </div>
+        <div class="time">${cityTime.format(
+          "h:mm:ss"
+        )} <small>${cityTime.format("A")}</small></div>
+    </div>
+  `;
 }
 
 updateCityTime();
